e9026cb1bbd587caedf2987e8776e1de0e76e65c
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-12-06 11:30:29 -0500

    mapped through players to return player card

---
+++ client/src/components/PlayerList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PlayerCard from './PlayerCard';
+
+const PlayerList = props => {
+    return (
+        <div>
+            {props.players.map(player =>(
+                <PlayerCard key={player.id} player={player}/>
+            ))}
+        </div>
+    )
+}
