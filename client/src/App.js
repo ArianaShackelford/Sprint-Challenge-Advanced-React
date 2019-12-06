b0d1fa38e03dab03220349b88ac472787ca9e000
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-12-06 11:24:57 -0500

    made App a class component, and called axios.get

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  state = {
+    players: []
+  }
+
+componentDidMount(){
+  axios.get('http://localhost:5000/api/players')
+    .then(response => {
+      console.log(response)
+      this.setState({players: response.data})
+    })
+}
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Player info</h1>
+     <PlayerList players={this.state.players}/>
     </div>
-  );
+  )};
 }
 
 export default App;
